540a7e3db5ba7af490543ff9532b2375a0720084
Kolotyluyk <ower1995@ukr.net>
2017-01-27 17:37:52 +0200

    Initial commit create model channel create model category create model programs

---
+++ app/src/main/java/com/sk/tvschedule/model/Category.java
@@ -0,0 +1,46 @@
+package com.sk.tvschedule.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Сергій on 27.01.2017.
+ */
+
+public class Category {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("picture")
+    @Expose
+    private String picture;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+}
---
+++ app/src/main/java/com/sk/tvschedule/model/Programs.java
@@ -0,0 +1,68 @@
+package com.sk.tvschedule.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Сергій on 27.01.2017.
+ */
+
+public class Programs  {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("picture")
+    @Expose
+    private String picture;
+    @SerializedName("category_id")
+    @Expose
+    private Integer categoryId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+}
